40b3945c5cb7e692930b68176b8f35891a338f51
Shisho Hackman <shisho.hackman@gmail.com>
2023-01-17 09:49:51 +0000

    add articles/:article_id/comments endpoint

--- controllers/index.js
+++ controllers/index.js
@@ -2,6 +2,8 @@ const {
   fetchTopics,
   fetchArticles,
   fetchArticleById,
+  fetchComments,
+  addComment,
 } = require("../models/index");
 
 exports.getTopics = (req, res, next) => {
@@ -21,7 +23,7 @@ exports.getArticles = (req, res, next) => {
     .then(({ rows }) => {
       let message = "here are the articles";
       if (rows.length === 0) {
-        res.status(404).send({ message: "article not found" });
+        res.status(404).send({ message: "Article not found" });
       } else {
         let articles = rows;
         articles.forEach((article) => {
@@ -36,16 +38,47 @@ exports.getArticles = (req, res, next) => {
 exports.getArticleById = (req, res, next) => {
   const { article_id } = req.params;
   if (!/^\d+$/.test(article_id)) {
-    res.status(400).send({ msg: "invalid id" });
+    res.status(400).send({ message: "Invalid ID" });
   } else {
     fetchArticleById(article_id)
-      .then(({ rows }) => {
+      .then((article) => {
+        res.status(200).send({ article });
+      })
+      .catch(next);
+  }
+};
+
+exports.getComments = (req, res, next) => {
+  const { article_id } = req.params;
+  if (!/^\d+$/.test(article_id)) {
+    res.status(400).send({ message: "Invalid ID" });
+  } else {
+    Promise.all([fetchComments(article_id), fetchArticleById(article_id)])
+      .then(([{ rows }, result2]) => {
         if (rows.length === 0) {
-          res.status(404).send({ message: "article not found" });
+          res.status(200).send({ message: "No comments", comments: [] });
         } else {
-          const article = rows[0];
-          res.status(200).send({ article });
+          rows.article_id = +article_id;
+          res.status(200).send({ comments: rows });
         }
+      })
+      .catch(next);
+  }
+};
+
+exports.postComments = (req, res, next) => {
+  const { article_id } = req.params;
+  const author = req.body.username;
+  const body = req.body.body;
+  if (!/^\d+$/.test(article_id)) {
+    res.status(400).send({ message: "Invalid ID" });
+  } else {
+    Promise.all([
+      addComment(article_id, author, body),
+      fetchArticleById(article_id),
+    ])
+      .then(([newComment, result2]) => {
+        res.status(201).send({ "comment added": newComment });
       })
       .catch(next);
   }
